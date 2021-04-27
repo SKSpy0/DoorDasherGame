6ad12fb7f81da724b0e0a60e48d9bfa1d5cf49d0
William Lee <wiklee@ucsc.edu>
2021-04-27 16:12:13 -0700

    player placeholder asset added, playScene has player and cone added for scale checking

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,11 +5,18 @@ class Play extends Phaser.Scene{
 
     preload() {
         this.load.image('background', './assets/background.png');
+        this.load.image('playerSprite', './assets/player.png');
+        this.load.image('cone','./assets/Cone.png');
     }
 
     create(){
         this.add.text(20, 20, "Door Dasher Play");
+        
         this.background = this.add.tileSprite(0, 0, 720, 480, 'background').setOrigin(0, 0);
+
+        this.player = this.add.sprite(100, 100, 'playerSprite').setOrigin(0,0);
+
+        this.cone = this.add.sprite(100, 150, 'cone').setOrigin(0,0);
     }
 
     update() {
