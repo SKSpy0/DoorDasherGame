b752a6e0199b26228dba2d8fd873386070db51b2
William Lee <wiklee@ucsc.edu>
2021-04-28 18:50:12 -0700

    play.js will store level data

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,19 +3,29 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
 
+    init(data){
+        this.level = data;
+    }
+
     preload() {
+        //load background
         this.load.image('background', './assets/background.png');
+
+        //load player
         this.load.image('playerSprite', './assets/player.png');
+
+        //load obstacles
         this.load.image('cone','./assets/Cone.png');
     }
 
     create(){
-        this.add.text(20, 20, "Door Dasher Play");
-        
+        //place background
         this.background = this.add.tileSprite(0, 0, 720, 480, 'background').setOrigin(0, 0);
 
+        //place player
         this.player = this.add.sprite(100, 100, 'playerSprite').setOrigin(0,0);
-
+        
+        
         this.cone = this.add.sprite(100, 150, 'cone').setOrigin(0,0);
     }
 
