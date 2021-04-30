3962501a2192b1a58286f119981e444f6f13ac00
timothy-tai <titai@ucsc.edu>
2021-04-30 03:27:10 -0700

    Added jumping

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,18 +1,36 @@
 class Player extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
+        // Adds physics elements to player
         scene.physics.add.existing(this);
+        
+        // Adds player to scene
         this.scene.add.existing(this);
-        this.enableBody(true, x, y, true, true);
-        this.acceleration = 1000;
-        this.drag = 3000;
+
+        // Set initial values
+        this.acceleration = 700;
+        this.drag = 4000;
+        this.jump = -500;
+        this.maxXVelocity = 500;
+        this.maxYvelocity = 5000;
+        this.setMaxVelocity(this.maxXVelocity, this.maxYvelocity);
     }
 
     update() {
         if(cursors.left.isDown) {
             this.body.setAccelerationX(-this.acceleration);
+            // Lets you jump while holding left
+            if(this.body.touching.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+                this.body.setVelocityY(this.jump);
+            }
         } else if(cursors.right.isDown) {
             this.body.setAccelerationX(this.acceleration);
+            // Lets you jump while holding right
+            if(this.body.touching.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+                this.body.setVelocityY(this.jump);
+            }
+        } else if(this.body.touching.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            this.body.setVelocityY(this.jump);
         } else {
             this.body.setAccelerationX(0);
             this.body.setDragX(this.drag);
