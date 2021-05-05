ebda6d81603703bd632cf0a7759b2addd92692f5
timothy-tai <titai@ucsc.edu>
2021-05-04 20:20:28 -0700

    added to game over scene

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -5,17 +5,33 @@ class GameOver extends Phaser.Scene{
 
     preload(){
         this.load.image('gameOver', './assets/gameOver.png');
-        //this.load.audio('endbgm', './assets/gameOver.mp3');
+        this.load.audio('endbgm', './assets/gameOverNoise.mp3');
     }
 
     create(){
         this.cameras.main.fadeIn(1000, 0, 0, 0);
-        /*
-        this.startbgm = this.sound.add('startbgm', {
-            loop:true,
+        this.endbgm = this.sound.add('endbgm', {
+            loop:false,
             volume: 0.5
         });
-        */
+        this.endbgm.play();
         this.add.tileSprite(0, 0, 720, 480, 'gameOver').setOrigin(0,0);
+
+        this.time.delayedCall(5000, () => {
+            this.add.text(centerWidth, centerHeight+200, 'Press UP to Restart', { fontFamily: 'CustomFont', fontSize: '20px', color: 'red'}).setOrigin(0.5);
+		})
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        this.keyPressed = false;
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyUP) && this.keyPressed == false) {
+            this.cameras.main.fadeOut(1000, 0, 0, 0);
+            this.endbgm.pause();
+            this.keyPressed = true;
+            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                this.scene.start('startScene');
+            });
+        }
     }
 }
