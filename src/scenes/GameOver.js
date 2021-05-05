433c214198a8189eac45e763f26ef34e235ef5be
William Lee <wiklee@ucsc.edu>
2021-05-04 21:00:50 -0700

    gameover scene changes highscore if needed

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -9,6 +9,27 @@ class GameOver extends Phaser.Scene{
     }
 
     create(){
+        // check for high score in local browser storage , taken from Nathan's Paddle Parkour
+        if(localStorage.getItem('hiscore') != null){
+            let storedScore = parseInt(localStorage.getItem('hiscore'));
+            //check if current score is higher than stored
+            if(level > storedScore){
+                console.log('new high score!')
+                localStorage.setItem('hiscore', level.toString());
+                highScore = level;
+                newHighScore = true;
+            } else {
+                console.log('no new high score...');
+                highScore = parseInt(localStorage.getItem('hiscore'));
+                newHighScore = false;
+            }
+        } else {
+            console.log('no high score saved, creating a new one');
+            highScore = level;
+            localStorage.setItem('hiscore', highScore.toString());
+            newHighScore = true;
+        }
+
         this.cameras.main.fadeIn(1000, 0, 0, 0);
         /*
         this.startbgm = this.sound.add('startbgm', {
